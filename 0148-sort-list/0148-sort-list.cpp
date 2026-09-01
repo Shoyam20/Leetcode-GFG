ad84f70c8be2dd0d47754dac4a116c7494b8aed6
Shoyam20 <Shoyamshoyam302@gmail.com>
2026-09-01 19:29:11 +0530

    Time: 18 ms (54.37%), Space: 56.9 MB (90.38%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,26 +10,78 @@
  */
 class Solution {
 public:
-    ListNode* sortList(ListNode* head) {
-        ListNode*temp=head;
+    ListNode* findmid(ListNode *start )
+    {
+        ListNode *slow=start;
+        ListNode *fast=start->next;
+
+        while(fast!=NULL && fast->next!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+
+        return slow;
+    }
+    ListNode * merge(ListNode *lefthead,ListNode*righthead)
+    {
+        // ListNode *dummy = new ListNode(-1);
+        // ListNode *temp = dummy; 
+        ListNode dummy(-1);
+ListNode *temp = &dummy;
+
+        while(lefthead!=NULL && righthead!=NULL)
+        {
+            if(lefthead->val < righthead->val)
+            {
+                temp->next=lefthead;
+                temp=temp->next;
+                lefthead=lefthead->next;
+            }
+            else{
+                temp->next=righthead;
+                temp=temp->next;
+                righthead=righthead->next;
+            }
+        }
+        while(lefthead!=NULL)
+        {
+            temp->next=lefthead;
+            temp=temp->next;
+            lefthead=lefthead->next;
+        }
+        while(righthead!=NULL)
+        {
+            temp->next=righthead;
+            temp=temp->next;
+            righthead=righthead->next;
+        }
+
+        return dummy.next;
+    }
+    ListNode *ms(ListNode *head)
+    {
         if(head==NULL || head->next==NULL) return head;
-        vector<int> v;
-        while(temp!=NULL)
-        {
-            v.push_back(temp->val);
-            temp=temp->next;
-        }
+
+        ListNode * middle=findmid(head);
+
+        ListNode *lefthead=head;
+        ListNode *righthead=middle->next;
+        middle->next=NULL;
+
+        lefthead=ms(lefthead);
+        righthead=ms(righthead);
+
+        return merge(lefthead,righthead);
+    }
+    ListNode* sortList(ListNode* head) {
+        
+
+        if(head==NULL || head->next==NULL) return head;
+        return ms(head);
 
 
-        sort(v.begin(),v.end());
 
-        temp=head;
-        int i=0;
-        while(temp!=NULL)
-        {
-            temp->val=v[i++];
-            temp=temp->next;
-        }
-        return head;
+        
     }
 };
