fa2719af0ecd999a3fbdfcff9b8d080b92cdacf9
Shoyam20 <Shoyamshoyam302@gmail.com>
2026-09-14 19:26:04 +0530

    Time: 2 ms (32.64%), Space: 23.3 MB (6.14%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        vector<int> ans;
-        stack<int> st;
-
+        // vector<int> ans;
+        // stack<int> st;
+        list<int> st;
         for(auto i : asteroids)
         {
-            int val=i;
-            int flag=0;
-            while(!st.empty() && val<0){
-                int t=st.top();
-                if(t<0) break;
-                if(t<(-1*val))
-                {
-                    st.pop();
+            if(i>0) st.push_back(i);
+            else{
+                while(!st.empty() && st.back()>0 && st.back() < abs(i)){
+                    st.pop_back();
                 }
-                else if(t==(-1*val)){
-                    st.pop();
-                    flag=1;
-                    break;
-                }
-                else{
-                    flag=1;
-                    break;
+
+                if(!st.empty() && st.back()==abs(i))
+                    st.pop_back();
+                else if(st.empty() || st.back()<0 ){
+                    st.push_back(i);
                 }
             }
-            if(flag==1) continue;
-            st.push(i);
         }
+            
+        vector<int> ans(st.begin(),st.end());
 
-        while(!st.empty()){
-            ans.push_back(st.top());
-            st.pop();
-        }
-        reverse(ans.begin(),ans.end());
+        // while(!st.empty()){
+        //     ans.push_back(st.top());
+        //     st.pop();
+        // }
+        // reverse(ans.begin(),ans.end());
         return ans;
     }
 };
